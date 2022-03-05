8de8b50a227eff9a286cca8d88fbedc1d79ee9c9
codenamehemingway <jared.hemingway.jh@gmail.com>
2022-03-05 14:19:08 -0700

    continue code

--- Develop/server.js
+++ Develop/server.js
@@ -5,11 +5,24 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 
 app.get("/notes", (req, res) => {
-  res.sendFile;
+  res.sendFile(path.join(__dirname, "./public/index.html"));
 });
 
-app.get("/api/notes", (req, res) => {});
+app.get("/api/notes", (req, res) => {
+  const dataNotes = fs.readFileSync(
+    path.join(__dirname, "./db/db.json"),
+    "utf-8"
+  );
+  const parseNotes = JSON.parse(dataNotes);
+  res.json(parseNotes);
+});
 
+app.post("/api/notes", (req, res) => {});
+
+
+app.delete("/api/notes/:id", (req, res) => {
+    let noteIndex = dbJson.findIndex((note) =>
+});
 app.listen(PORT, () => {
   console.log(`API server now on port ${PORT}!`);
 });
