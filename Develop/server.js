d2107f80cf8a9301748673cca62ba5ad4276d67b
codenamehemingway <jared.hemingway.jh@gmail.com>
2022-03-05 15:44:39 -0700

    connected js.html.css

--- Develop/server.js
+++ Develop/server.js
@@ -1,8 +1,14 @@
 const fs = require("fs");
 const dbJson = require("./db/db.json");
 const express = require("express");
+const path = require("path");
+const { v1: uuidv1 } = require("uuid");
+
 const app = express();
 const PORT = process.env.PORT || 3001;
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
 
 app.get("/notes", (req, res) => {
   res.sendFile(path.join(__dirname, "./public/index.html"));
@@ -17,11 +23,38 @@ app.get("/api/notes", (req, res) => {
   res.json(parseNotes);
 });
 
-app.post("/api/notes", (req, res) => {});
+app.post("/api/notes", (req, res) => {
+  const dataNotes = fs.readFileSync(
+    path.join(__dirname, "./db/db.json"),
+    "utf-8"
+  );
+  const parseNotes = JSON.parse(dataNotes);
+  req.body.id = uuidv1();
+  parseNotes.push(req.body);
 
-
+  fs.writeFileSync(
+    path.join(__dirname, "./db/db.json"),
+    JSON.stringify(parseNotes),
+    "utf-8"
+  );
+  res.json("Note Added!!!");
+});
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "./public/index.html"));
+});
 app.delete("/api/notes/:id", (req, res) => {
-    let noteIndex = dbJson.findIndex((note) =>
+  let noteIndex = dbJson.findIndex((note) => note.id == req.params.id); //find the index of a note of it has an id equal to the passed in id
+  if (noteIndex) {
+    dbJson.splice(noteIndex, 1); //then take out that one note starting at that index position
+    fs.writeFile("./db/db.json", JSON.stringify(dbJson), (err, data) => {
+      if (err) {
+        console.log("Data is : " + data);
+        throw err;
+      }
+    });
+    return res.status(200).json("Note deleted.");
+  }
+  return res.status(404).json("Note not found.");
 });
 app.listen(PORT, () => {
   console.log(`API server now on port ${PORT}!`);
